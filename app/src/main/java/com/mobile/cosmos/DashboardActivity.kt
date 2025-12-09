d98d5a2667f7b85a952f177281d19de01b43f2bb
girija <girijaprasadbhattarai322@gmail.com>
2025-12-09 19:49:03 +0545

    Initial Commit

---
+++ app/src/main/java/com/mobile/cosmos/DashboardActivity.kt
@@ -0,0 +1,70 @@
+package com.mobile.cosmos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.topAppBar)) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(
+                view.paddingLeft,
+                systemBars.top,
+                view.paddingRight,
+                systemBars.bottom
+            )
+
+            insets
+        }
+
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navView = findViewById(R.id.navigationView)
+
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(topAppBar)
+
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+
+        // Handle Drawer Menu Clicks
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> Toast.makeText(this,
+                    "Home Clicked", Toast.LENGTH_SHORT).show()
+
+                R.id.nav_profile -> Toast.makeText(this,
+                    "Profile Clicked", Toast.LENGTH_SHORT).show()
+
+                R.id.nav_courses -> Toast.makeText(this,
+                    "Courses Clicked", Toast.LENGTH_SHORT).show()
+
+                R.id.nav_settings -> Toast.makeText(this,
+                    "Settings Clicked", Toast.LENGTH_SHORT).show()
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+    }
+}
---
+++ app/src/main/java/com/mobile/cosmos/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.mobile.cosmos
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
